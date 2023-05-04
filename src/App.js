b915463d35b167a4edaabab3a55fdb9a04243a27
VeraMatias <veramatias93@hotmail.com>
2023-05-03 21:36:44 -0300

    Creacion de ItemCount

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import './App.css';
 import NavBar from './components/NavBar/Navbar';
 
 
 import ItemListContainer from './container/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
   return (
     <>
       <NavBar />
       <ItemListContainer greeting={"Bienvenidos a Habia Una Vez"} />
+      <ItemCount  initial={0} stock={4} onAdd={(count) => console.log("Cantidad:",count)}/>
+
     </>
   );
 }
---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+import './ItemCount.css';
+
+const ItemCount =  ({initial, stock, onAdd}) => {
+    const [counter, setCounter] = useState(initial);
+    
+    const handlerDecrease = () => {
+        if (counter > 0){ setCounter ( counter - 1);}
+    }
+
+    const handlerIncrease = () => {
+        if (counter < stock){ setCounter ( counter + 1);}
+    }
+
+    return(
+        <div className="container-count">
+            <div className="count-controls">
+                <button onClick={handlerDecrease} className="controls-btn">-</button>
+                <h4 className="controls-number">{counter}</h4>
+                <button onClick={handlerIncrease}  className="controls-btn">+</button>
+            </div>
+            <button className="count-add" onClick={() => onAdd(counter)} disabled={!stock || counter==0}>Agregar al Carrito</button>
+        </div>
+    )
+}
+export default ItemCount
