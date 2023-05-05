e418622ec8e2520b3bac527a54421fa39a3ff601
VeraMatias <veramatias93@hotmail.com>
2023-05-05 19:52:26 -0300

    Agregado de estilos a ItemDetails y modificacion margin itemcount

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,16 +1,24 @@
+import './ItemDetail.css';
+
 import ItemCount from '../../components/ItemCount/ItemCount';
 
 const ItemDetail =  ({product}) => {
     return(
         <>
+            <h2 className='product-name'>{product.nombre}</h2>
             <div className="container-details">
-                <div className="details-product">
-                    <h2 className='product-name'>{product.nombre}</h2>
+                <div className="details-product-left">
                     <img className="product-image" src={product.imagen} alt="imagen del producto" />
-                    <p className='product-description'>{product.descripcion}</p>
-                    <p className='product-price'>{product.precio}</p>
                 </div>
-                <ItemCount  initial={0} stock={product.stock} onAdd={(count) => console.log("Cantidad:",count)}/>
+                <div className="details-product-right">
+                    <div className='container-description'>
+                        <p className='description-title'>Descripción del Producto</p>
+                        <p className='product-description'>{product.descripcion}</p>
+                        <p className='product-price'>Precio: ${product.precio}</p>
+                    </div>
+                    <p className='count-title'>Agregar al carrito</p>
+                    <ItemCount  initial={0} stock={product.stock} onAdd={(count) => console.log("Cantidad:",count)}/>
+                </div>
             </div>
         </>
     )
