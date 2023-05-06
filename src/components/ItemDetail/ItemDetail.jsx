5b84daf5e34be43e22aa4e4ca41cc4dfb5a6f653
VeraMatias <veramatias93@hotmail.com>
2023-05-06 20:43:41 -0300

    Modificacion ancho y alto skeletons ItemDetail

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -12,11 +12,11 @@ const ItemDetail =  ({product}) => {
                 <div className="details-product-right">
                     <div className='container-description'>
                         <p className='description-title'>Descripción del Producto</p>
-                        <p className='product-description'>{product.descripcion || <Skeleton count={5}/>}</p>
-                        {!product.precio ? <Skeleton /> : <p className='product-price'>Precio: ${product.precio}</p> }
+                        <p className='product-description'>{product.descripcion || <Skeleton count={5} width={"30rem"}/>}</p>
+                        {!product.precio ? <Skeleton width={"10rem"} height={"2rem"}/> : <p className='product-price'>Precio: ${product.precio}</p> }
                     </div>
                     <p className='count-title'>Agregar al carrito</p>
-                    {!product.stock ? <Skeleton/> : <ItemCount  initial={0} stock={product.stock} onAdd={(count) => console.log("Cantidad:",count)}/>}
+                    {!product.stock ? <Skeleton height={"2rem"}/> : <ItemCount  initial={0} stock={product.stock} onAdd={(count) => console.log("Cantidad:",count)}/>}
                 </div>
             </div>
         </>
