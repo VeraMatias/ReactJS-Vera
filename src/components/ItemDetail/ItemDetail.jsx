4b7f2cc4ed033a26f2f8955983be51b9da528a43
VeraMatias <veramatias93@hotmail.com>
2023-05-06 20:40:20 -0300

    Agregado de Skeleton a ItemDetail

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,23 +1,22 @@
 import './ItemDetail.css';
 
 import ItemCount from '../../components/ItemCount/ItemCount';
+import Skeleton from 'react-loading-skeleton';
 
 const ItemDetail =  ({product}) => {
     return(
         <>
-            <h2 className='product-name'>{product.nombre}</h2>
+            <h2 className='product-name'>{product.nombre || <Skeleton/>}</h2>
             <div className="container-details">
-                <div className="details-product-left">
-                    <img className="product-image" src={product.imagen} alt="imagen del producto" />
-                </div>
+                {!product.imagen ? <Skeleton height={500} width={700}/> : <img className="product-image" src={product.imagen} alt="imagen del producto" />}
                 <div className="details-product-right">
                     <div className='container-description'>
                         <p className='description-title'>Descripción del Producto</p>
-                        <p className='product-description'>{product.descripcion}</p>
-                        <p className='product-price'>Precio: ${product.precio}</p>
+                        <p className='product-description'>{product.descripcion || <Skeleton count={5}/>}</p>
+                        {!product.precio ? <Skeleton /> : <p className='product-price'>Precio: ${product.precio}</p> }
                     </div>
                     <p className='count-title'>Agregar al carrito</p>
-                    <ItemCount  initial={0} stock={product.stock} onAdd={(count) => console.log("Cantidad:",count)}/>
+                    {!product.stock ? <Skeleton/> : <ItemCount  initial={0} stock={product.stock} onAdd={(count) => console.log("Cantidad:",count)}/>}
                 </div>
             </div>
         </>
