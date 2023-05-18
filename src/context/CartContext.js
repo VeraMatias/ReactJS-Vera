cfaafcd685661b45523e36db7f5ddd2fd04bad04
VeraMatias <veramatias93@hotmail.com>
2023-05-18 16:09:04 -0300

    Creacion de CustomHook UseCart y modificacion de alerta cuando no queda stock para agregar

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,47 +1,13 @@
-import { createContext, useState } from "react";
+import { createContext} from "react";
+import { useCart } from "../hooks/useCart";
 
 export const CartContext = createContext([])
 
 export const CartProvider = ({children}) => {
-    const [cart, setCart] = useState([])
 
-    const addItem = (item, quantity) =>{
-        if(!isInCart(item.id)){
-            setCart(prev => [...prev, {item,quantity}])
-        }else{
-            const quantityPrev = cart.find(itemPrev => itemPrev.item.id === item.id).quantity
-            if (item.stock >= (quantityPrev + quantity)){
-                removeItem(item.id)
-                quantity += quantityPrev
-                setCart(prev => [...prev, {item,quantity}])
-            }else{
-                alert(`La cantidad sobrepasa el stock actual. Solo puede agregar ${item.stock - quantityPrev} unidades más.`)
-            }
-        }
-        console.log("Cantidad",quantityCArt())
-    }
-
-    const removeItem = (itemID) =>{
-        const newCart = cart.filter(itemFilter => itemFilter.item.id !== itemID)
-        setCart(newCart)
-    }
-
-    const clearCart = () =>{
-        setCart([])
-    }
-
-    const isInCart =(itemID) =>{
-        return cart.some(itemSome => itemSome.item.id === itemID)
-    }
-
-    const quantityCArt = () =>{
-        let quantityTotal = 0
-        cart.forEach(item => quantityTotal += item.quantity)
-        return (quantityTotal)
-    }
-
+    const methodsCart = useCart()
     return(
-        <CartContext.Provider value = {{cart, addItem, removeItem, clearCart}}>
+        <CartContext.Provider value = {methodsCart}>
             {children}
         </CartContext.Provider>
     )
---
+++ src/hooks/useCart.js
@@ -0,0 +1,43 @@
+import { useState } from "react"
+
+
+export const useCart = () =>{
+    const [cart, setCart] = useState([])
+
+    const addItem = (item, quantity) =>{
+        if(!isInCart(item.id)){
+            setCart(prev => [...prev, {item,quantity}])
+        }else{
+            const quantityPrev = cart.find(itemPrev => itemPrev.item.id === item.id).quantity
+            if (item.stock >= (quantityPrev + quantity)){
+                removeItem(item.id)
+                quantity += quantityPrev
+                setCart(prev => [...prev, {item,quantity}])
+            }else{
+                console.log(item.stock - quantityPrev)
+                item.stock - quantityPrev > 0 ? alert(`La cantidad sobrepasa el stock actual. Solo puede agregar ${item.stock - quantityPrev} unidades más.`) : alert("No queda Stock disponible")
+            }
+        }
+    }
+
+    const removeItem = (itemID) =>{
+        const newCart = cart.filter(itemFilter => itemFilter.item.id !== itemID)
+        setCart(newCart)
+    }
+
+    const clearCart = () =>{
+        setCart([])
+    }
+
+    const isInCart =(itemID) =>{
+        return cart.some(itemSome => itemSome.item.id === itemID)
+    }
+
+    const quantityCart = () =>{
+        let quantityTotal = 0
+        cart.forEach(item => quantityTotal += item.quantity)
+        return (quantityTotal)
+    }
+
+    return {cart, addItem, removeItem, clearCart, quantityCart}
+}
